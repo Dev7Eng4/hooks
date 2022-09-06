1c8e10c50dcdb8e92f15dfb2c8636b5a3e4434b7
Dev7Eng4 <duongbich9n7@gmail.com>
2022-09-06 17:15:16 +0700

    update note interview

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Effect from './components/Effect';
 import NoRerender from './components/NoRerender';
+import RightClick from './components/RightClick';
 
 function App() {
+  const [name, setName] = useState('A');
+
+  const handleClick = () => {
+    setName((prev) => `${prev}N`);
+  };
   return (
     <div className='App'>
+      <RightClick defaultName={name} />
       <Effect />
       <NoRerender />
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>Click</button>
     </div>
   );
 }
